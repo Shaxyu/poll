e3ec4d4f924b0682360d584cf4dd1409f1f8a87a
ShaxYu <brian930516@gmail.com>
2020-04-30 12:03:40 +0800

    投票主題列表:苦功版

--- default/views.py
+++ default/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import poll
 
 # Create your views here.
+def poll_list(req):
+    polls = poll.objects.all()
+    return render(req, 'poll_list.html', {'poll_list':polls})
+
